7ee246751ebb39599d518c33c76d738ba29fe7ea
Paras Sharma <paraazz@outlook.com>
2016-05-15 20:32:50 +0530

    db install files added

--- find_me/db_install/install.php
+++ find_me/db_install/install.php
@@ -1,18 +1,39 @@
+
 <?php
+  
+$localhost = $_POST['localhost'];
+$username = $_POST['username'];
+$password = $_POST['pass'];
 
 
-$data_base = "CREATE DATABASE sggs_db_secure";
 
-$create_table = "CREATE TABLE `sggs_db_secure`.`events` ( `title` VARCHAR(100) NOT NULL , `date` VARCHAR(10) NOT NULL , `day` VARCHAR(15) NOT NULL , `place` VARCHAR(51) NOT NULL ) ENGINE = InnoDB";
+$link = mysqli_connect($localhost, $username, $password);
+$sql_db = "CREATE DATABASE sggs_db_secure";
+$sql_table = "CREATE TABLE `sggs_db_secure`.`events` ( `title` VARCHAR(100) NOT NULL , `date` VARCHAR(10) NOT NULL , `day` VARCHAR(15) NOT NULL , `place` VARCHAR(51) NOT NULL ) ENGINE = InnoDB";
 
-if ($data_base && $create_table) {
- echo "Install Complete Now delete the find_me folder";   
-    
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+ 
 
+
+if(mysqli_query($link, $sql_db) && mysqli_query($link, $sql_table)){
+    echo "Database created successfully";
+} 
 else {
- echo "Hey, It failed. No prob.. Try again.. Never Give UP!";   
-    
+    echo "Failed! Try Again:: $sql. " . mysqli_error($link);
 }
+ 
 
+mysqli_close($link);
 ?>
+
+
+
+
+
+
+
+
+
