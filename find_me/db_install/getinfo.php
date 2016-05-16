574b1aba4238cd3033b18084268b994201407e3b
parassharmaa <paraazz@outlook.com>
2016-05-16 18:27:55 +0530

    update

--- find_me/db_install/getinfo.php
+++ find_me/db_install/getinfo.php
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
+
 
 <form action="install" method="POST">
 <input type="text" name="localhost">
 <input type="text" name="username">
 <input type="text" name="pass">
 <input type="submit">
-=======
-
-<form action="install" method="POST">
-<input type="text" name="localhost">
-<input type="text" name="username">
-<input type="text" name="pass">
-<input type="submit">
->>>>>>> 7ee246751ebb39599d518c33c76d738ba29fe7ea
 </form>
